39b6d2e2a301205a951654c48e31305ae37e3c11
Elurr <jonatikkan@gmail.com>
2022-11-08 12:27:27 +0100

    crono funcional completo

--- frontend/JavaScript/ejercicios/cronómetro/script.js
+++ frontend/JavaScript/ejercicios/cronómetro/script.js
@@ -5,8 +5,20 @@ let cents = document.getElementById('cents');
 let contadorCents = 0;
 let contadorSecs = 0;
 let contadorMins = 0;
+let crono;
+
+// let btnStart = document.getElementById('btnStart');
 // Botón START
 // El cronómetro arranca
+function startCrono() {
+    // Detenemos el intervalo o bucle para evitar errores
+    clearInterval(crono);
+
+    // Un intervalo se ejecuta en bucle cada x(10 en este caso) milisegundos
+    crono = setInterval(start, 10);
+}
+
+// Función que cuenta el tiempo y lo imprime
 function start() {
     // Aumento el contador en 1 por que la función se ejecuta cada centésima de segundo
     contadorCents++;
@@ -36,10 +48,32 @@ function start() {
         segundos.innerHTML = '0' + contadorSecs;
     }
     if (contadorMins < 10) {
-        minutos.innerHTML = '0' + contadorMins; 
+        minutos.innerHTML = '0' + contadorMins;
     }
 
     // La función se ejecuta cada 10ms (1 centésima de segundo)
-    setTimeout(start, 10)
+
+
+    // btnStart.disabled = true;
+}
+
+// Detenemos el bucle
+function stop() {
+    clearInterval(crono);
+    // btnStart.disabled = false;
+}
+
+// Detenemos el bucle y reiniciamos la interfaz y los contadores
+function reset() {
+    clearInterval(crono);
+    // btnStart.disabled = false;
+
+    contadorCents = 0;
+    contadorSecs = 0;
+    contadorMins = 0;
+
+    minutos.innerHTML = '00';
+    segundos.innerHTML = '00';
+    cents.innerHTML = '00';
 }
 
