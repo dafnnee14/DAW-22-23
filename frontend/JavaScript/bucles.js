defed2a9165c50d29a5523d3464e874cd9f93028
Elurr <jonatikkan@gmail.com>
2022-11-11 12:47:10 +0100

    bucles

--- frontend/JavaScript/bucles.js
+++ frontend/JavaScript/bucles.js
@@ -9,6 +9,10 @@ for (let i = 2; i < 20; i = i + 2) {
     texto += i + " ";
 }
 
+for (let i = 0; i < 20; i = i + 2) {
+    arrayNumeros[i] = i;
+}
+
 display.innerHTML = texto;
 console.log(arrayNumeros)
 
@@ -28,3 +32,39 @@ for (let i = 0; i < letras.length; i++) {
 }
 
 displayLetras.innerHTML = letras.join(' ');
+
+
+// LLenar un array con números pares
+let pares = [];
+
+for (let i = 1; i <= 10; i++) {
+    pares[i - 1] = i * 2;
+
+}
+
+console.log(pares);
+display.innerHTML = pares;
+
+
+// Imprimir los múltiplos del 3 hasta el 100
+texto = '';
+
+for (let i = 3; i < 100; i = i + 3) {
+    texto += i + ", "
+
+}
+
+display.innerHTML = texto;
+
+
+// Lo mismo pero cuenta atrás desde el 100
+texto = '';
+
+for (let i = 99; i >= 3; i = i - 3) {
+
+    texto += i + "<br>";
+}
+
+display.innerHTML = texto;
+// display.innerHTML = pares.join('<br>');
+// display.innerHTML = arrayNumeros;
